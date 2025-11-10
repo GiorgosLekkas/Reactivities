e9f04caee471331c3e66f0ecf8596a736bb6fc6b
Γιώργος Λέκκας <george200023@gmail.com>
2025-11-11 00:56:14 +0200

    new commit

---
+++ API/Controllers/ActivityController.cs
@@ -0,0 +1,48 @@
+using Application.Activities.Commands;
+using Application.Activities.DTOs;
+using Application.Activities.Queries;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/activities")]
+    public class ActivityController: BaseApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await Mediator.Send(new GetActivityList.Query());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivityDetail(string id)
+        {
+            return await Mediator.Send(new GetActivityDetails.Query { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> CreateActivity(CreateActivityDto activityDto)
+        {
+            return await Mediator.Send(new CreateActivity.Command { ActivityDto = activityDto });
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<string>> EditActivity(Activity activity)
+        {
+            await Mediator.Send(new EditActivity.Command { Activity = activity });
+            return NoContent();
+        }
+        
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Activity>> DeleteActivity(string id)
+        {
+            await Mediator.Send(new DeleteActivity.Command { Id = id });
+
+            return Ok();
+        }
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,56 @@
+using API.Middleware;
+using Application.Activities;
+using Application.Activities.Queries;
+using Application.Core;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// builder.Services.AddDbContext<AppDbContext>(opt =>
+// {
+//     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+// });
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseMySql(
+        builder.Configuration.GetConnectionString("MySqlString"),
+        new MySqlServerVersion(new Version(8, 0, 36)) // Βάλε τη δική σου έκδοση MySQL
+    );
+});
+
+builder.Services.AddCors();
+builder.Services.AddMediatR(x => {
+    x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>();
+    x.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+builder.Services.AddValidatorsFromAssemblyContaining<CreateActivityValidator>();
+builder.Services.AddTransient<ExceptionMiddleware>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
+
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DbInitializer.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration.");
+}
+
+app.Run();
